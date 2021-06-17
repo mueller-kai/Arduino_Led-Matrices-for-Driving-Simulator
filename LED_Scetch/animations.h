90c742f5945752a10b32c135140ee088f4396b6f
Kai Müller <kai.mueller@student.reutlingen-university.de>
2021-06-17 15:05:30 +0200

    acceted changes

--- LED_Scetch/animations.h
+++ LED_Scetch/animations.h
@@ -45,20 +45,12 @@ void itterthroughleds(uint8_t red, uint8_t green, uint8_t blue, uint8_t wait)
 
 
 //arrow_right
-<<<<<<< HEAD
-void arrow_right(uint8_t red, uint8_t green, uint8_t blue, uint8_t thickness)
-=======
 void arrow_right(uint8_t red, uint8_t green, uint8_t blue, uint8_t thickness, uint8_t animation_speed)
->>>>>>> a3bb365870d1b6a00bdc4ad94eda338aa99f40af
 {
   //The Animation will only count to 32, because if an instance is animated 32 columns to thr right is is exactly in the place of the next arrow.
   //For example: The first arrow is not visible in the matrix because it is drawn in the buffer zone, but if it is animated 32 columns o the right it is exactly in the position of the second arrow.
   //At this point all arrows (execpt the fourth) have reached the position of their right neighbour arrow, therefore the animation can be started again from 0.
-<<<<<<< HEAD
-  for(uint8_t animation = 0; animation < 32; animation ++)
-=======
   for(uint8_t animation = 0; animation < 32; animation = animation + animation_speed)
->>>>>>> a3bb365870d1b6a00bdc4ad94eda338aa99f40af
   {
     //drawing 4 instances of the arrow
     for(uint8_t instance = 0; instance < 4; instance++)
@@ -92,18 +84,12 @@ void arrow_right(uint8_t red, uint8_t green, uint8_t blue, uint8_t thickness, ui
 
 
 //Animate a left arrow on Matrix
-<<<<<<< HEAD
-void arrow_left(uint8_t red, uint8_t green, uint8_t blue, uint8_t thickness)
-{
-  for(uint8_t animation = 0; animation < 32; animation++)
-=======
 void arrow_left(uint8_t red, uint8_t green, uint8_t blue, uint8_t thickness, uint8_t animation_speed)
 {
   //The Animation will only count to 32, because if an instance is animated 32 columns to thr right is is exactly in the place of the next arrow.
   //For example: The first arrow is not visible in the matrix because it is drawn in the buffer zone, but if it is animated 32 columns o the right it is exactly in the position of the second arrow.
   //At this point all arrows (execpt the fourth) have reached the position of their right neighbour arrow, therefore the animation can be started again from 0.
   for(uint8_t animation = 0; animation < 32; animation = animation + animation_speed)
->>>>>>> a3bb365870d1b6a00bdc4ad94eda338aa99f40af
   {
     //draw 4 instances of an arrow
     for(uint8_t instance = 0; instance < 4; instance++)
@@ -123,11 +109,7 @@ void arrow_left(uint8_t red, uint8_t green, uint8_t blue, uint8_t thickness, uin
         {
           for(uint8_t col = col_count - (32 - row); col > col_count - (32 - row) - thickness -1 ; col--)
           {
-<<<<<<< HEAD
-            unsort_leds.setPixelColor((sort_leds[rowcol(row, col - animation - (instance * 32))]), unsort_leds.Color(red, green, blue));
-=======
             unsort_leds.setPixelColor((sort_leds[rowcol(row, col - animation - (instance * 32) + 1)]), unsort_leds.Color(red, green, blue));//+1 to ensure symtery
->>>>>>> a3bb365870d1b6a00bdc4ad94eda338aa99f40af
           }//- row_count - matrixbuffer + row + 1 - animation - (instance_of_arrow * 32)
         }
       }
@@ -139,28 +121,16 @@ void arrow_left(uint8_t red, uint8_t green, uint8_t blue, uint8_t thickness, uin
 
 
 //animate arrow top right
-<<<<<<< HEAD
-void arrow_topright(uint8_t red, uint8_t green, uint8_t blue,uint8_t thickness)
-{
-  //wideness from left to right
-  uint8_t wideness = 16;
-  for(uint8_t animation = 0; animation < 32; animation++) 
-=======
 void arrow_topright(uint8_t red, uint8_t green, uint8_t blue,uint8_t thickness, uint8_t animation_speed)
 {
   //wideness from left to right
   uint8_t wideness = 16;
   for(uint8_t animation = 0; animation < 32; animation = animation + animation_speed) 
->>>>>>> a3bb365870d1b6a00bdc4ad94eda338aa99f40af
   {
     for(uint8_t instance = 0; instance < 3; instance++)
     {
       //print a box
-<<<<<<< HEAD
-      for(uint8_t row = 32; row < row_count; row ++)
-=======
       for(uint8_t row = 32; row < row_count -1; row ++) //1 row from bottom is cropped to ensure symetry
->>>>>>> a3bb365870d1b6a00bdc4ad94eda338aa99f40af
       {
         for(uint8_t col = 0; col < wideness; col++)
         {
@@ -185,31 +155,18 @@ void arrow_topright(uint8_t red, uint8_t green, uint8_t blue,uint8_t thickness, 
 
 
 //animate arrow pointing top left
-<<<<<<< HEAD
-void arrow_topleft(uint8_t red, uint8_t green, uint8_t blue, uint8_t thickness)
-{
-  uint8_t wideness = 16;
-  //animate the arrow
-  for(uint8_t animation = 0; animation < 32; animation++)
-=======
 void arrow_topleft(uint8_t red, uint8_t green, uint8_t blue, uint8_t thickness, uint8_t animation_speed)
 {
   uint8_t wideness = 16;
   //animate the arrow
   for(uint8_t animation = 0; animation < 32; animation = animation + animation_speed)
->>>>>>> a3bb365870d1b6a00bdc4ad94eda338aa99f40af
   {
     //to avoid the first arrow to be animated and drawn instance will start at 1.
     for(uint8_t instance = 1; instance < 4; instance++)
     {    
       
-<<<<<<< HEAD
-      //draw the arrow
-      for(uint8_t row = 32; row < row_count; row++)
-=======
       //draw A BOX
       for(uint8_t row = 32; row < row_count -1; row++) //1 row from bottom is cropped to ensure symetry
->>>>>>> a3bb365870d1b6a00bdc4ad94eda338aa99f40af
       {
         for(uint8_t col = 0; col < wideness; col++)
         {
@@ -234,28 +191,16 @@ void arrow_topleft(uint8_t red, uint8_t green, uint8_t blue, uint8_t thickness, 
 
 
 //animate arrow pointing down right
-<<<<<<< HEAD
-void arrow_downright(uint8_t red, uint8_t green, uint8_t blue,uint8_t thickness)
-{
-  //wideness from left to right
-  uint8_t wideness = 16;
-  for(uint8_t animation = 0; animation < 32; animation++) 
-=======
 void arrow_downright(uint8_t red, uint8_t green, uint8_t blue,uint8_t thickness, uint8_t animation_speed)
 {
   //wideness from left to right
   uint8_t wideness = 16;
   for(uint8_t animation = 0; animation < 32; animation = animation + animation_speed) 
->>>>>>> a3bb365870d1b6a00bdc4ad94eda338aa99f40af
   {
     for(uint8_t instance = 0; instance < 3; instance++)
     {
       //print a box
-<<<<<<< HEAD
-      for(uint8_t row = 0; row < row_count - 32; row ++)
-=======
       for(uint8_t row = 1; row < row_count - 32; row ++)//cropp row 0 from arrow to ensure symetry
->>>>>>> a3bb365870d1b6a00bdc4ad94eda338aa99f40af
       {
         for(uint8_t col = 0; col < wideness; col++)
         {
@@ -280,29 +225,17 @@ void arrow_downright(uint8_t red, uint8_t green, uint8_t blue,uint8_t thickness,
 
 
 //animate arrow pointing down left
-<<<<<<< HEAD
-void arrow_downleft(uint8_t red, uint8_t green, uint8_t blue,uint8_t thickness)
-{
-  //wideness from left to right
-  uint8_t wideness = 16;
-  for(uint8_t animation = 0; animation < 32; animation++) 
-=======
 void arrow_downleft(uint8_t red, uint8_t green, uint8_t blue,uint8_t thickness, uint8_t animation_speed)
 {
   //wideness from left to right
   uint8_t wideness = 16;
   for(uint8_t animation = 0; animation < 32; animation = animation + animation_speed) 
->>>>>>> a3bb365870d1b6a00bdc4ad94eda338aa99f40af
   {
     //to keep the first arrow from beeing drawn, instance starts at 1
     for(uint8_t instance = 1; instance < 4; instance++)
     {
       //print a box
-<<<<<<< HEAD
-      for(uint8_t row = 0; row < row_count - 32; row ++)
-=======
       for(uint8_t row = 1; row < row_count - 32; row ++)//1row is croped to ensure symetry
->>>>>>> a3bb365870d1b6a00bdc4ad94eda338aa99f40af
       {
         for(uint8_t col = 0; col < wideness; col++)
         {
@@ -327,29 +260,17 @@ void arrow_downleft(uint8_t red, uint8_t green, uint8_t blue,uint8_t thickness, 
 
 
 //animate an arrows down
-<<<<<<< HEAD
-void arrow_down(uint8_t red, uint8_t green, uint8_t blue, uint8_t thickness)
-=======
 void arrow_down(uint8_t red, uint8_t green, uint8_t blue, uint8_t thickness, uint8_t animation_speed)
->>>>>>> a3bb365870d1b6a00bdc4ad94eda338aa99f40af
 {
   uint8_t arrow_height = 16;
   uint8_t arrow_width = 32;
   
-<<<<<<< HEAD
- for(uint8_t animation = 0; animation < 32; animation++)
-=======
  for(uint8_t animation = 0; animation < 32; animation = animation + animation_speed)
->>>>>>> a3bb365870d1b6a00bdc4ad94eda338aa99f40af
  {
       for(uint8_t instances = 0; instances < 4; instances++)
       {
           //Draw a box
-<<<<<<< HEAD
-          for(uint8_t row = 0; row < arrow_height; row++)
-=======
           for(uint8_t row = 1; row < arrow_height; row++)
->>>>>>> a3bb365870d1b6a00bdc4ad94eda338aa99f40af
           {
             for(uint8_t col = matrixbuffer; col < 48; col ++)
             {
@@ -392,20 +313,12 @@ void arrow_down(uint8_t red, uint8_t green, uint8_t blue, uint8_t thickness, uin
 
 
 //Draw an arrow straight ahead
-<<<<<<< HEAD
-void arrow_ahead(uint8_t red, uint8_t green, uint8_t blue, uint8_t thickness)
-=======
 void arrow_ahead(uint8_t red, uint8_t green, uint8_t blue, uint8_t thickness, uint8_t animation_speed)
->>>>>>> a3bb365870d1b6a00bdc4ad94eda338aa99f40af
 {
   uint8_t arrow_height = 16;
   uint8_t arrow_width = 32;
 
-<<<<<<< HEAD
-         for(uint8_t animation = 0; animation < 32; animation++)
-=======
          for(uint8_t animation = 0; animation < 33; animation = animation + animation_speed)
->>>>>>> a3bb365870d1b6a00bdc4ad94eda338aa99f40af
          {
             for(uint8_t instances = 0; instances < 4; instances++)
             {
@@ -422,11 +335,7 @@ void arrow_ahead(uint8_t red, uint8_t green, uint8_t blue, uint8_t thickness, ui
               //darken pixels to create a triangle
               for(uint8_t row = 32; row < row_count; row++)
               {
-<<<<<<< HEAD
-                for(uint8_t col = matrixbuffer; col <  arrow_width + (arrow_width - row); col++)
-=======
                 for(uint8_t col = matrixbuffer; col <  arrow_width + (arrow_width - row) - 1; col++)
->>>>>>> a3bb365870d1b6a00bdc4ad94eda338aa99f40af
                 {
                   unsort_leds.setPixelColor((sort_leds[rowcol(row - animation, col+ (instances*32))]), unsort_leds.Color(0, 0, 0));
                 }
